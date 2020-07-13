e25f52c9d1b2a8fbfef2deaa8452b94d8c89d829
Brian Kim <briaansjkim@gmail.com>
2020-07-13 19:12:39 -0400

    addede check for ingredients length

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -6,14 +6,19 @@ import classes from './Burger.css';
 const Burger = (props) => {
     // passed in props is an object so we have to transform it into an array
     // igKey will be each ingredient
-    const transformedIngredients = Object.keys(props.ingredients).map(igKey => {
+    let transformedIngredients = Object.keys(props.ingredients).map(igKey => {
         // props.ingredients[igKey] is the number of ingredients 
         // creating an array with a length of the number of each given ingredient
         return [...Array(props.ingredients[igKey])].map((_, i) => {
             return <BurgerIngredient key={igKey + i} type={igKey} />;
         });
-    });
+    }).reduce((arr, el) => {
+        return arr.concat(el);
+    }, []);
 
+    if (transformedIngredients.length === 0) {
+        transformedIngredients = <p>Please start adding ingredients</p>;
+    }
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top" />
