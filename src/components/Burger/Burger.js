43dedacaabe8872bb768bae1d90cd3e3eb286db2
Brian Kim <briaansjkim@gmail.com>
2020-07-13 16:11:33 -0400

    added init burger

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 import classes from './Burger.css';
 
 const Burger = (props) => {
     return (
-        <div></div>
+        <div className={classes.Burger}>
+            <BurgerIngredient type="bread-top" />
+            <BurgerIngredient type="cheese" />
+            <BurgerIngredient type="meat" />
+            <BurgerIngredient type="bread-bottom" />
+        </div>
     );
 };
 
