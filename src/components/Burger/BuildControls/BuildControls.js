c03a310f9e0d9864fa54a4f7ebdcabd16b7d9698
Brian Kim <briaansjkim@gmail.com>
2020-07-14 16:56:09 -0400

    passed down ingredient handler

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -13,7 +13,11 @@ const controls = [
 const BuildControls = (props) => (
     <div className={classes.BuildControls}>
         {controls.map(control =>
-            <BuildControl label={control.label} key={control.label} />
+            <BuildControl 
+                label={control.label} 
+                key={control.label} 
+                added={() => props.ingredientAdded(control.type)}
+            />
         )}
     </div>
 );
