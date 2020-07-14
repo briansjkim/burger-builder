7fffb4fb7b6cf96e1c298fcc271c37dd0981dc3f
Brian Kim <briaansjkim@gmail.com>
2020-07-14 16:37:23 -0400

    added controls to add ingredients

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -2,6 +2,13 @@ import React from 'react';
 
 import classes from './BuildControls.css';
 
+const controls = [
+    { label: 'Salad', type: 'salad' },
+    { label: 'Bacon', type: 'bacon' },
+    { label: 'Cheese', type: 'cheese' },
+    { label: 'Meat', type: 'meat' },
+]
+
 const BuildControls = (props) => (
     <div>
 
