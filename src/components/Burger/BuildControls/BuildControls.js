ff15e22e5aa5cc437e369ec8990914918d4dcbf6
Brian Kim <briaansjkim@gmail.com>
2020-07-14 21:27:13 -0400

    added button for order and styling

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -22,6 +22,7 @@ const BuildControls = (props) => (
                 disabled={props.disabled[control.type]}
             />
         )}
+        <button className={classes.OrderButton}>Order Now</button>
     </div>
 );
 
