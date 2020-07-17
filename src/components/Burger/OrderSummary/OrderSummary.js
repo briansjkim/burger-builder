606232cbda2049a6eb370d1622f9a26973ac0934
Brian Kim <briaansjkim@gmail.com>
2020-07-17 10:06:08 -0700

    init ordersummary component

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 const OrderSummary = (props) => {
+    const ingredientSummary = Object.keys(props.ingredients).map(igKey => {
+        return <li><span style={{ textTransform: 'capitalize' }}>{igKey}</span>: {props.ingredients[igKey]}</li>
+    });
 
+    < Fragment >
+        <h3>Your Order</h3>
+        <p>A delicious burger with the following ingredients:</p>
+        <ul>
+            {ingredientSummary}
+        </ul>
+        <p>Continue to Checkout</p>
+    </Fragment >
 };
 
 export default OrderSummary;
