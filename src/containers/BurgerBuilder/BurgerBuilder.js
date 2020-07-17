86fc25679452cb7847217811fde947070cb16702
Brian Kim <briaansjkim@gmail.com>
2020-07-17 11:52:39 -0700

    added purchase handler to send alert for now

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -69,7 +69,11 @@ class BurgerBuilder extends Component {
 
     purchaseCancelHandler = () => {
         this.setState({ purchasing: false });
-    }
+    };
+
+    purchaseContinueHandler = () => {
+        alert('You continue');
+    };
 
     render() {
         const disabledInfo = {
@@ -84,7 +88,11 @@ class BurgerBuilder extends Component {
         return (
             <Fragment>
                 <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
-                    <OrderSummary ingredients={this.state.ingredients} />
+                    <OrderSummary
+                        ingredients={this.state.ingredients}
+                        purchaseCancelled={this.purchaseCancelHandler}
+                        purchaseContinued={this.purchaseContinueHandler}
+                    />
                 </Modal>
                 <Burger ingredients={this.state.ingredients} />
                 <BuildControls
