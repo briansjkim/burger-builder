5f0eac100fc04078cfdbd49a8d5a892a51a28420
Brian Kim <briaansjkim@gmail.com>
2020-07-17 11:53:09 -0700

    edited click handler for button

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -5,7 +5,7 @@ import classes from './Button.css';
 const Button = (props) => (
     <button
         className={[classes.Button, classes[props.btnType]].join(' ')}
-        onClick={props.click}
+        onClick={props.clicked}
     >{props.children}</button>
 );
 
