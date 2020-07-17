1133caaa7be329088c8f4f76c65b903bd541335e
Brian Kim <briaansjkim@gmail.com>
2020-07-17 10:09:57 -0700

    completed ordersummary component

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -2,7 +2,11 @@ import React, { Fragment } from 'react';
 
 const OrderSummary = (props) => {
     const ingredientSummary = Object.keys(props.ingredients).map(igKey => {
-        return <li><span style={{ textTransform: 'capitalize' }}>{igKey}</span>: {props.ingredients[igKey]}</li>
+        return (
+            <li key={igKey}>
+                <span style={{ textTransform: 'capitalize' }}>{igKey}</span>: {props.ingredients[igKey]}
+            </li>
+        );
     });
 
     return (
