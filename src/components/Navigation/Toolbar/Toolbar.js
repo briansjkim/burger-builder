c2aca8d7a90d438289038c0646336ee5dc041538
Brian Kim <briaansjkim@gmail.com>
2020-07-21 13:18:55 -0700

    added building blocks

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,5 +1,13 @@
 import React from 'react';
 
-const Toolbar = (props) => ();
+const Toolbar = (props) => (
+    <header>
+        <div>MENU</div>
+        <div>LOGO</div>
+        <nav>
+            ...
+        </nav>
+    </header>
+);
 
 export default Toolbar;
