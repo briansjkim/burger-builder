894c80f5a25a510a0c88530a4938188b8aba980d
Brian Kim <briaansjkim@gmail.com>
2020-07-21 13:34:21 -0700

    used image and added css

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import burgerLogo from '../../assets/images/burger-logo.png';
+
+const Logo = (props) => (
+    <div>
+        <img src={burgerLogo} alt="My Burger" />
+    </div>
+);
+
+export default Logo;
