9a718aeca0f9ebbe486ea6b4ad564749dd3f1d71
Brian Kim <briaansjkim@gmail.com>
2020-07-21 13:22:54 -0700

    imported css using CSS modules

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
+import classes from './Toolbar.css';
+
 const Toolbar = (props) => (
-    <header>
+    <header className={classes.Toolbar}>
         <div>MENU</div>
         <div>LOGO</div>
         <nav>
