3cbda2a0dd38e11670f38eec15c6c9db480e4259
Brian Kim <briaansjkim@gmail.com>
2020-08-03 14:13:39 -0400

    converted component from functional to class

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -3,6 +3,10 @@ import React, { Fragment, Component } from 'react';
 import Button from '../../UI/Button/Button';
 
 class OrderSummary extends Component {
+    componentWillUpdate() {
+        console.log('[OrderSummary] WillUpdate')
+    }
+
     render() {
         const ingredientSummary = Object.keys(this.props.ingredients).map(igKey => {
             return (
