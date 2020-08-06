ebe99a0920c31e2bac256195993ea1912cd960b6
Brian Kim <briaansjkim@gmail.com>
2020-08-06 14:37:22 -0400

    edited axios request statements

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -75,7 +75,7 @@ class BurgerBuilder extends Component {
     };
 
     purchaseContinueHandler = () => {
-        // alert('You continue');
+        this.setState({ loading: true });
         const order = {
             ingredients: this.state.ingredients,
             // normally, you'd calculate the price in the server
@@ -94,8 +94,12 @@ class BurgerBuilder extends Component {
 
         // .json is the endpoint you need for firebase to work properly
         axios.post('/orders.json', order)
-            .then(response => console.log(response))
-            .catch(error => console.log(error));
+            .then(response => {
+                this.setState({ loading: false, purchasing: false });
+            })
+            .catch(error => {
+                this.setState({ loading: false, purchasing: false });
+            });
     };
 
     render() {
