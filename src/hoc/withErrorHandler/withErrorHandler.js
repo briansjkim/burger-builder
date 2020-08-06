5b205377e9e48129101adf7070ab32192c5a7571
Brian Kim <briaansjkim@gmail.com>
2020-08-06 15:09:28 -0400

    testing witherrorhandler component

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -11,8 +11,9 @@ const withErrorHandler = (WrappedComponent, axios) => {
         componentDidMount() {
             axios.interceptors.request.use(req => {
                 this.setState({ error: null })
+                return req;
             })
-            axios.interceptors.response.use(null, error => {
+            axios.interceptors.response.use(res => res, error => {
                 this.setState({ error: error });
             });
         }
@@ -26,7 +27,7 @@ const withErrorHandler = (WrappedComponent, axios) => {
                 <Fragment >
                 <Modal 
                     show={this.state.error}
-                    clicked={this.errorConfirmedHandler}
+                    modalClosed={this.errorConfirmedHandler}
                 >
                     {this.state.error ? this.state.error.message : null}
                 </Modal>
