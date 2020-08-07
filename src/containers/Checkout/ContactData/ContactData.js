03d4bb40c1464b466622ef2ed47e5a5d3292974b
Brian Kim <briaansjkim@gmail.com>
2020-08-07 16:47:40 -0400

    init contactdata

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -1,7 +1,31 @@
 import React, { Component } from 'react';
 
-class ContactData extends Component {
+import Button from '../../../components/UI/Button/Button';
 
+class ContactData extends Component {
+    state = {
+        name: '',
+        email: '',
+        address: {
+            street: '',
+            postalCode: ''
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <h4>Enter your Contact Data</h4>
+                <form>
+                    <input type="text" name="name" placeholder="Your Name"></input>
+                    <input type="email" name="email" placeholder="Your Email"></input>
+                    <input type="text" name="street" placeholder="Street"></input>
+                    <input type="text" name="postal" placeholder="Postal Code"></input>
+                    <Button btnType="Success">ORDER</Button>
+                </form>
+            </div>
+        );
+    }
 };
 
 export default ContactData;
