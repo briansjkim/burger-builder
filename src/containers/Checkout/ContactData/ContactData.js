5f933f60ee9658294e1172d10d2f77788227ecda
Brian Kim <briaansjkim@gmail.com>
2020-08-10 17:00:06 -0400

    edited contactdata component

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -106,7 +106,7 @@ class ContactData extends Component {
                     key={formElement.id}
                     elementType={formElement.config.elementType} 
                     elementConfig={formElement.config.elementConfig} 
-                    value={formElement.config.elementConfig}
+                    value={formElement.config.value}
                 />
             ))}
             <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
