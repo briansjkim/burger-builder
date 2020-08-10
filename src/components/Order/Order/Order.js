ac8a0a7b7ffcb1971e665b65141a46ac3d8b3259
Brian Kim <briaansjkim@gmail.com>
2020-08-10 13:29:14 -0400

    added styling and init component

--- src/components/Order/Order/Order.js
+++ src/components/Order/Order/Order.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
+import classes from './Order.css';
+
 const Order = (props) => (
-    <div>
+    <div className={classes.Order}>
         <p>Ingredients: Salad (1)</p>
         <p>Price: <strong>USD 5.45</strong></p>
     </div>
