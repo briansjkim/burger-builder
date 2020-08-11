69fd0a95ec7bfa2b4fedb02b3723e36c4f8ee295
Brian Kim <briaansjkim@gmail.com>
2020-08-11 15:48:20 -0400

    edited logic for form element

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -4,16 +4,21 @@ import classes from './Input.css';
 
 const Input = (props) => {
     let inputElement = null;
+    const inputClasses = [classes.InputElement];
+
+    if (props.invalid) {
+        inputClasses.push(classes.Invalid);
+    };
 
     switch (props.elementType) {
         case ('input'):
-            inputElement = <input className={classes.InputElement} {...props.elementConfig} value={props.value} onChange={props.changed} />;
+            inputElement = <input className={inputClasses.join(' ')} {...props.elementConfig} value={props.value} onChange={props.changed} />;
             break;
         case ('textarea'):
-            inputElement = <textarea className={classes.InputElement} {...props.elementConfig} value={props.value} onChange={props.changed} />;
+            inputElement = <textarea className={inputClasses} {...props.elementConfig} value={props.value} onChange={props.changed} />;
             break;
         case ('select'):
-            inputElement = <select className={classes.InputElement} value={props.value}onChange={props.changed} >
+            inputElement = <select className={inputClasses} value={props.value}onChange={props.changed} >
                 {props.elementConfig.options.map(option => (
                     <option key={option.value} value={option.value}>
                         {option.displayValue}
@@ -21,7 +26,7 @@ const Input = (props) => {
                 ))}</select>;
             break;
         default:
-            inputElement = <input className={classes.InputElement} {...props.elementConfig} value={props.value} onChange={props.changed} />;
+            inputElement = <input className={inputClasses} {...props.elementConfig} value={props.value} onChange={props.changed} />;
     }
 
     return (
