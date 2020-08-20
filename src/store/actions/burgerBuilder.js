82c20ae020d4ad7062d7a376f91fb814606ea01c
Brian Kim <briaansjkim@gmail.com>
2020-08-20 14:16:06 -0400

    added actions for building a buger

--- src/store/actions/burgerBuilder.js
+++ src/store/actions/burgerBuilder.js
@@ -1,1 +1,16 @@
 // action creator for building a burger
+import * as actionTypes from './actionTypes';
+
+export const addIngredient = (name) => {
+    return {
+        type: actionTypes.ADD_INGREDIENT,
+        ingredientName: name
+    };
+};
+
+export const removeIngredient = (name) => {
+    return {
+        type: actionTypes.REMOVE_INGREDIENT,
+        ingredientName: name
+    };
+};
