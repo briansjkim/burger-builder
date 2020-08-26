68480e23104db977276643530419c71e39b75e07
Brian Kim <briaansjkim@gmail.com>
2020-08-26 11:43:26 -0400

    working on logic for reducer

--- src/store/reducers/order.js
+++ src/store/reducers/order.js
@@ -6,5 +6,19 @@ const initialState = {
     loading: false
 };
 
-const reducer = (state, action) => {
-}
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case actionTypes.PURCHASE_BURGER_SUCCESS:
+            return {
+
+            };
+        case actionTypes.PURCHASE_BURGER_FAIL:
+            return {
+
+            };
+        default:
+            return state;
+    };
+};
+
+export default reducer;
