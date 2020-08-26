b4c3bc012725ab0c99dd97da9e7d25c2bcd2a3f6
Brian Kim <briaansjkim@gmail.com>
2020-08-26 11:41:36 -0400

    init orders reducer

--- src/store/reducers/order.js
+++ src/store/reducers/order.js
@@ -1,1 +1,10 @@
 // reducer for submitting orders
+import * as actionTypes from '../actions/actionTypes';
+
+const initialState = {
+    orders: [],
+    loading: false
+};
+
+const reducer = (state, action) => {
+}
