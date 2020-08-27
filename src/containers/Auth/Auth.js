459cfce5132db735ad0deec35648a513322c3b9b
Brian Kim <briaansjkim@gmail.com>
2020-08-27 11:40:14 -0400

    imported components to use

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 
+import Input from '../../components/UI/Input/Input';
+import Button from '../../components/UI/Button/Button';
+
 class Auth extends Component {
     state = {
         controls: {
@@ -35,6 +38,13 @@ class Auth extends Component {
     };
 
     render() {
+        const formElementsArray = [];
+        for (let key in this.state.orderForm) {
+            formElementsArray.push({
+                id: key,
+                config: this.state.orderForm[key]
+            });
+        };
         return (
             <div>
                 <form>
