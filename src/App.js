af13043245036c0f9b154f90319b74c076d02e65
Brian Kim <briaansjkim@gmail.com>
2020-09-04 16:40:43 -0400

    added more security by guarding routes

--- src/App.js
+++ src/App.js
@@ -16,19 +16,35 @@ class App extends Component {
     };
 
     render() {
+        let routes = (
+            <Switch>
+                <Route path="/auth" component={Auth} />
+                <Route path="/" exact component={BurgerBuilder} />
+            </Switch>
+        );
+
+        if (this.props.isAuthenticated) {
+            routes = (
+                <Switch>
+                    <Route path="/checkout" component={Checkout} />
+                    <Route path="/orders" component={Orders} />
+                    <Route path="/logout" component={Logout} />
+                </Switch>
+            );
+        }
         return (
             <div>
                 <Layout>
-                    <Switch>
-                        <Route path="/checkout" component={Checkout} />
-                        <Route path="/orders" component={Orders} />
-                        <Route path="/auth" component={Auth} />
-                        <Route path="/logout" component={Logout} />
-                        <Route path="/" exact component={BurgerBuilder} />
-                    </Switch>
+                    {routes}
                 </Layout>
             </div>
         );
+    };
+};
+
+const mapStateToProps = state => {
+    return {
+        isAuthenticated: state.auth.token !== null
     };
 };
 
@@ -38,4 +54,4 @@ const mapDispatchToProps = dispatch => {
     };
 };
 
-export default withRouter(connect(null, mapDispatchToProps)(App));
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
