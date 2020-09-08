b1e1d2ff57bbe4533e52ec6978b642f5fff2a4da
Brian Kim <briaansjkim@gmail.com>
2020-09-08 12:47:15 -0400

    added testing suite for a NavigationItems

---
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+// shallow should be used as often as possible bc it renders the component with all contents but the contents aren't deeply rendered
+// this means that other components that have their own contents are only rendered as placeholders
+// this allows us to isolate components for testing
+import NavigationItems from './NavigationItems';
+import NavigationItem from './NavigationItem/NavigationItem';
+
+configure({ adapter: new Adapter() });
+
+
+describe('<NavigationItems />', () => {
+    let wrapper;
+    beforeEach(() => {
+        wrapper = shallow(<NavigationItems />);
+    });
+    // function that will automatically be executed before each test
+    it('should render two <NavigationItem /> elements if not authenticated', () => {
+        expect(wrapper.find(NavigationItem)).toHaveLength(2);
+    });
+
+    it('shoulder render three <NavigationItem /> elements if authenticated', () => {
+        wrapper.setProps({ isAuthenticated: true });
+        expect(wrapper.find(NavigationItem)).toHaveLength(3);
+    });
+});
